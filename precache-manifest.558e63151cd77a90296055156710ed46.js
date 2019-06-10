a2eb1ae843c6a5c3762ea1010b4d9e42bb851e42
Olegus <gulldan1996@gmail.com>
2019-06-10 18:57:43 +0300

    Updates

---
+++ precache-manifest.558e63151cd77a90296055156710ed46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8595e693d1adb796df9bf777a011f2f1",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "83de9c3410e92778ea21",
+    "url": "/react_dynamic-list-of-posts/static/css/main.b7f9fb6e.chunk.css"
+  },
+  {
+    "revision": "4e46e9d638db29490995",
+    "url": "/react_dynamic-list-of-posts/static/js/2.20e50229.chunk.js"
+  },
+  {
+    "revision": "83de9c3410e92778ea21",
+    "url": "/react_dynamic-list-of-posts/static/js/main.cbc3fca8.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
